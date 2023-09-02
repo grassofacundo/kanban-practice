9e96dde8254f0a8cc9df0ae913971f50f9e93cbc
Facundo Grasso <facundofggrasso@gmail.com>
2023-09-02 18:41:17 -0300

    Initialize db service

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
 import { FunctionComponent } from "react";
 import { ContextLoader } from "./contexts/ContextLoader";
 import Dashboard from "./dashboard/Dashboard";
+import dragAndDropService from "../services/dragAndDropService";
 
 const App: FunctionComponent = () => {
+    dragAndDropService.init();
+
     return (
         <ContextLoader>
             <Dashboard />
